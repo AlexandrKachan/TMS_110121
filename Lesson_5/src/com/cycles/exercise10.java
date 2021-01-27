835d1fd5d7c50a856f75252e02e13a227392fd80
Alexandr <kachan.alexandr@gmail.com>
2021-01-27 15:38:13 +0300

    ex 10.

---
+++ Lesson_5/src/com/cycles/exercise10.java
@@ -0,0 +1,31 @@
+package com.cycles;
+
+import com.sun.org.apache.xpath.internal.res.XPATHErrorResources_sv;
+
+public class exercise10 {
+
+    public static void main(String[] args) {
+
+
+        // Посчитать факториал числа в границах от 10 до 15
+
+        int a = 10;
+        int b = a;
+        int c= 15;
+        System.out.print("число произведения" +b);
+
+        while ( b<=c){
+            a = a*b;
+            b++;
+            System.out.println(" произведение = " + a);
+            System.out.print(" число произведения" +b);
+        }
+
+
+    }
+}
+
+
+
+
+

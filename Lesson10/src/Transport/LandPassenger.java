439839e783c2f3fde31a2acab00aa74857424a8c
Alexandr <kachan.alexandr@gmail.com>
2021-02-22 17:52:37 +0300

    Transport

--- Lesson10/src/Transport/LandPassenger.java
+++ Lesson10/src/Transport/LandPassenger.java
@@ -7,27 +7,52 @@ public class LandPassenger extends LandTransport {
     public String getBrand() {
         return "Renault";
     }
+
     public double getPower() {
-        return 1.6;
+        return 180;
     }
+
     public double getSpeed() {
         return 200;
     }
+
     public double getWeight() {
         return 1900;
     }
+
     public int getWheels() {
         return 4;
     }
+
     public double getFuel() {
         return 8;
     }
+
+    public double powerKvt() {
+        return getPower() * 0.74;
+    }
+
+    //    private double useFuel() {
+//        double useFuel = distance/100*getFuel();
+//        return useFuel;
+//    }
+    public double fuelDist(double time) {
+        double distance = time * getSpeed();
+        double useFuel = distance / 100 * getFuel();
+
+        System.out.println("За время " + time + " часа, автомобиль " + getBrand() + "  двигаясь с максимальной скоростью "
+                + getSpeed() + " км/ч израсходует " + useFuel + "  литров топлива");
+
+        return useFuel;
+    }
+
+
     public void print() {
         System.out.println("Брэнд - " + getBrand() + " ,вес машины - " + getWeight()
-                + "килограмм , мощность - " + getPower() +"л/с  и киловат - " /*+ powerCalculation()+ */);
-        System.out.println("максимальная скорость - "+ getSpeed() + ", количество колес- " + getWheels() + ", потребление топлива - "
+                + " кг , мощность - " + getPower() + " л/с  или " + powerKvt() + " киловат");
+        System.out.println("максимальная скорость - " + getSpeed() + ", количество колес- " + getWheels() + ", потребление топлива - "
                 + getFuel());
-        System.out.println(", грузоподъемность - " + " , тип кузова  - " + type +
-                ", количество пасажиров - "+ passengers);
+        System.out.println("Грузоподъемность - " + " , тип кузова  - " + type +
+                ", количество пасажиров - " + passengers);
     }
 }
--- Lesson10/src/Transport/TransportRunner.java
+++ Lesson10/src/Transport/TransportRunner.java
@@ -5,6 +5,27 @@ public class TransportRunner {
 
         LandPassenger pas = new LandPassenger();
         pas.print();
+        pas.fuelDist(3);
+        System.out.println();
+
+        LandFreight fr = new LandFreight();
+        fr.print();
+        fr.carringFr(22);
+        System.out.println();
+
+        AirCivil ac = new AirCivil();
+        ac.print();
+        ac.airPassengers(100);
+        System.out.println();
+
+        AirMilitary am = new AirMilitary();
+        am.print();
+        am.shootMiss();
+        am.shootMiss();
+        am.shootMiss();
+        am.eject();
+
+        System.out.println();
 
 
     }
